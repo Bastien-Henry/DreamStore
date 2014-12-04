2e4e6e7d17d229fe81aa4f01530fb7f8d242a467
Flambard <flambard@flambard-HP-EliteBook-8560p.(none)>
2014-12-04 17:26:22 +0100

    fix bug and start test unit

--- src/DreamStore/SellerBundle/Tests/Controller/HomeControllerTest.php
+++ src/DreamStore/SellerBundle/Tests/Controller/HomeControllerTest.php
@@ -3,10 +3,20 @@
 namespace DreamStore\SellerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use DreamStore\SellerBundle\Controller\HomeController;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndexAction()
+    public function testStock()
     {
+        /*
+        $home = new HomeController();
+
+        $product = $this->getDoctrine()->getRepository('DreamStoreSellerBundle:Product')->findOneById(1);
+        $table = ['operation' => 'add', 'stock' => 5];
+        $productStock = 10;
+
+        $home->addStock($product, $productStock, $table);
+        */
     }
 }
