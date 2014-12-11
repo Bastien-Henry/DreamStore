0398072ca5e61989e9d760f078115c5bc78ca95a
Bastien Henry <bastien.henry@supinternet.fr>
2014-12-11 10:34:15 +0100

    finish paypal for cart

--- src/DreamStore/CustomerBundle/Controller/PaymentController.php
+++ src/DreamStore/CustomerBundle/Controller/PaymentController.php
@@ -19,8 +19,18 @@ class PaymentController extends Controller
     public function indexAction($id)
     {
         $table = $this->getRequest()->request->get('dreamstore_customerbundle_paymenttype');
-        $product = $this->getDoctrine()->getRepository('DreamStoreSellerBundle:Product')->findOneById($id);
-        $stock = $product->getStock();
+
+        if ($table !== null) 
+        {
+            $product = $this->getDoctrine()->getRepository('DreamStoreSellerBundle:Product')->findOneById($id);
+            $stock = $product->getStock();
+
+            if($stock - $table['quantite'] < 0)
+            {
+               return $this->redirect($this->generateUrl('dream_store_customer_homepage')); 
+            }
+        }
+        
 
 
         if($table['place'] == "cart")
@@ -43,14 +53,16 @@ class PaymentController extends Controller
             return $this->redirect($this->generateUrl('dream_store_customer_homepage'));
         }
 
-        if($stock-$table['quantite'] < 0)
-            return $this->redirect($this->generateUrl('dream_store_customer_homepage'));
+        
+            
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_POST,1);
 
-        $post_var = array(
+        if ($table !== null) 
+        {
+            $post_var = array(
                 "METHOD" => "SetExpressCheckout",
                 "USER" => $this->email,
                 "PWD" => $this->password,
@@ -61,19 +73,91 @@ class PaymentController extends Controller
                 "cancelUrl" => "http://localhost/DreamStore/web/app_dev.php/show/".$id
             );
 
-        $post_var['L_PAYMENTREQUEST_0_NAME0']=$product->getName();
-        $post_var['L_PAYMENTREQUEST_0_DESC0']=$product->getDescription();
-        $post_var['L_PAYMENTREQUEST_0_AMT0']=$product->getPrice();
-        $post_var['L_PAYMENTREQUEST_0_QTY0']=$table['quantite'];
-        $post_var['PAYMENTREQUEST_0_ITEMAMT']=$product->getPrice() * $table['quantite'];
-        $post_var['PAYMENTREQUEST_0_TAXAMT']=$product->getPrice() * 0.2 * $table['quantite'];
-        $post_var['PAYMENTREQUEST_0_SHIPPINGAMT']=4.00;
-        $post_var['PAYMENTREQUEST_0_HANDLINGAMT']=0.00;
-        $post_var['PAYMENTREQUEST_0_QTY'] = $table['quantite'];
-        $post_var['PAYMENTREQUEST_0_AMT']= 4.00 + $post_var['PAYMENTREQUEST_0_TAXAMT'] + ($product->getPrice() * $table['quantite']);
-        $post_var['PAYMENTREQUEST_0_CURRENCYCODE']="EUR";
-        $post_var['ALLOWNOTE']=1;
+            $post_var['L_PAYMENTREQUEST_0_NAME0']=$product->getName();
+            $post_var['L_PAYMENTREQUEST_0_DESC0']=$product->getDescription();
+            $post_var['L_PAYMENTREQUEST_0_AMT0']=$product->getPrice();
+            $post_var['L_PAYMENTREQUEST_0_QTY0']=$table['quantite'];
+            $post_var['PAYMENTREQUEST_0_ITEMAMT']=$product->getPrice() * $table['quantite'];
+            $post_var['PAYMENTREQUEST_0_TAXAMT']=$product->getPrice() * 0.2 * $table['quantite'];
+            $post_var['PAYMENTREQUEST_0_SHIPPINGAMT']=4.00;
+            $post_var['PAYMENTREQUEST_0_HANDLINGAMT']=0.00;
+            $post_var['PAYMENTREQUEST_0_QTY'] = $table['quantite'];
+            $post_var['PAYMENTREQUEST_0_AMT']= 4.00 + $post_var['PAYMENTREQUEST_0_TAXAMT'] + ($product->getPrice() * $table['quantite']);
+            $post_var['PAYMENTREQUEST_0_CURRENCYCODE']="EUR";
+            $post_var['ALLOWNOTE']=1;
+        }
+        else
+        {
+            $username = $this->get('security.context')->getToken()->getUser()->getUsername();
 
+            $carts = $this->getDoctrine()->getRepository('DreamStoreCustomerBundle:Historical')->findBy(array('user' => $username, 'status' => 'panier'));
+
+            if (count($carts) > 1) 
+            {
+                $quantity = $carts[1]->getQuantity() + $carts[0]->getQuantity();
+            }
+            else
+            {
+                $quantity = $carts[0]->getQuantity();
+            }
+
+            
+
+            $post_var = array(
+                "METHOD" => "SetExpressCheckout",
+                "USER" => $this->email,
+                "PWD" => $this->password,
+                "SIGNATURE" => $this->signature,
+                "VERSION" => 78,
+                "AMT" => $quantity,
+                "returnUrl" => "http://localhost/DreamStore/web/app_dev.php/payment/return",
+                "cancelUrl" => "http://localhost/DreamStore/web/app_dev.php/show/".$id
+            );
+            //first item
+            $post_var['L_PAYMENTREQUEST_0_NAME0']=$carts[0]->getProduct()->getName();
+            $post_var['L_PAYMENTREQUEST_0_DESC0']=$carts[0]->getProduct()->getDescription();
+            $post_var['L_PAYMENTREQUEST_0_AMT0']=$carts[0]->getProduct()->getPrice();
+            $post_var['L_PAYMENTREQUEST_0_QTY0']=$carts[0]->getQuantity();
+
+            if (count($carts) > 1) 
+            {
+                //second item
+                $post_var['L_PAYMENTREQUEST_0_NAME1']=$carts[1]->getProduct()->getName();
+                $post_var['L_PAYMENTREQUEST_0_DESC1']=$carts[1]->getProduct()->getDescription();
+                $post_var['L_PAYMENTREQUEST_0_AMT1']=$carts[1]->getProduct()->getPrice();
+                $post_var['L_PAYMENTREQUEST_0_QTY1']=$carts[1]->getQuantity();
+            }
+            
+
+            //le total
+            if (count($carts) > 1) 
+            {
+                $totalAMTItem = ($carts[0]->getProduct()->getPrice() * $carts[0]->getQuantity()) + ($carts[1]->getProduct()->getPrice() * $carts[1]->getQuantity());
+            }
+            else
+            {
+                $totalAMTItem = ($carts[0]->getProduct()->getPrice() * $carts[0]->getQuantity());
+            }
+            
+            $post_var['PAYMENTREQUEST_0_ITEMAMT']=$totalAMTItem;
+
+            $post_var['PAYMENTREQUEST_0_TAXAMT']=$totalAMTItem * 0.2;
+            $post_var['PAYMENTREQUEST_0_SHIPPINGAMT']=4.00;
+            $post_var['PAYMENTREQUEST_0_HANDLINGAMT']=0.00;
+            if (count($carts) > 1) 
+            {
+                $post_var['PAYMENTREQUEST_0_QTY'] = $carts[0]->getQuantity() + $carts[1]->getQuantity();
+            }
+            else
+            {
+                $post_var['PAYMENTREQUEST_0_QTY'] = $carts[0]->getQuantity();
+            }
+            
+            $post_var['PAYMENTREQUEST_0_AMT']= 4.00 + $post_var['PAYMENTREQUEST_0_TAXAMT'] + ($totalAMTItem);
+            $post_var['PAYMENTREQUEST_0_CURRENCYCODE']="EUR";
+
+            $post_var['ALLOWNOTE']=1;
+        }
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_var));
 
@@ -90,7 +174,23 @@ class PaymentController extends Controller
 
         $response = array();
         parse_str($server_output, $response);
-        $this->historicalAction($product, $table['quantite'], $response['TOKEN'], 'en cours');
+        if ($table !== null) 
+        {
+            $this->historicalAction($product, $table['quantite'], $response['TOKEN'], 'en cours');
+        }
+        else
+        {
+            foreach ($carts as $historic) 
+            {
+                $historic->setToken($response['TOKEN']);
+                $historic->setStatus('en cours');
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($historic);
+                $em->flush();
+
+            }
+            
+        }
         $link = $this->getPaypalRedirectUrl($response['TOKEN']);
         return $this->redirect($link);
     }
@@ -104,16 +204,19 @@ class PaymentController extends Controller
             $resultPayment = $this->capturePayment($_GET['PayerID'], $_GET['token']);
             if ($resultPayment['PAYMENTINFO_0_ACK'] == "Success") 
             {
-                $historic = $this->getDoctrine()->getRepository('DreamStoreCustomerBundle:Historical')->findOneByToken($_GET['token']);
-                $historic->setStatus('paye');
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($historic);
-                $em->flush();
-                $product = $historic->getProduct();
-                $quantity = $historic->getQuantity();
-
-                $data['result'] = true;
-                $this->editStockAction($product, $quantity);
+                $historic = $this->getDoctrine()->getRepository('DreamStoreCustomerBundle:Historical')->findByToken($_GET['token']);
+                foreach ($historic as $one) 
+                {
+                    $one->setStatus('paye');
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($one);
+                    $em->flush();
+                    $product = $one->getProduct();
+                    $quantity = $one->getQuantity();
+                    $data['result'] = true;
+                    $this->editStockAction($product, $quantity);
+                }
+                
                 return $this->render('DreamStoreCustomerBundle:Home:returnPayment.html.twig', $data);
             }
             else
@@ -164,7 +267,7 @@ class PaymentController extends Controller
 
     function capturePayment($payerID, $tokenID)
     {
-        $payment = $this->getDoctrine()->getRepository('DreamStoreCustomerBundle:Historical')->findOneByToken($tokenID);
+        $payment = $this->getDoctrine()->getRepository('DreamStoreCustomerBundle:Historical')->findBy(array('token' => $tokenID, 'status' => 'en cours'));
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
@@ -176,16 +279,20 @@ class PaymentController extends Controller
                 "PWD" => $this->password,
                 "SIGNATURE" => $this->signature,
                 "VERSION" => 78,
-                "TOKEN" => $payment->getToken(),
+                "TOKEN" => $tokenID,
                 "PAYERID" => $payerID,
                 "PAYMENTREQUEST_0_PAYMENTACTION" => "SALE",
                 "PAYMENTREQUEST_0_CURRENCYCODE" => "EUR"
             );
 
-        // $post_var['PAYMENTREQUEST_0_TAXAMT'] = $payment->getPrice() * 0.2 * $payment->getQuantity();
-        $tax = $payment->getPrice() * 0.2 * $payment->getQuantity();
+        $totalAMT = 0;
+        foreach ($payment as $arrayPayment) 
+        {
+            $totalAMT += $arrayPayment->getPrice();
+        }
+        $taxFinal = $totalAMT * 0.2;
 
-        $post_var['PAYMENTREQUEST_0_AMT'] = 4.00 + $tax + ($payment->getPrice() * $payment->getQuantity());
+        $post_var['PAYMENTREQUEST_0_AMT'] = 4.00 + $taxFinal + $totalAMT;
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_var));
 
